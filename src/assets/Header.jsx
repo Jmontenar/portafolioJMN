b76fe0f7c5e1806e60c8b02896de5883d9b743f4
Jmontenar <jmontenar@gmail.com>
2023-04-11 23:09:18 -0500

    second commit

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -7,67 +7,68 @@ import Logo from './img/LOGO_JORGE.png'
 const Header = () => {
     const navbar = useRef()
     const handleHamClick = () => {
-    navbar.current.classList.toggle('header__nav--close')    
+    navbar.current.classList.toggle('open')    
     
   }
   return (
-<header className="header">
-      <NavLink className="header__logo_link"  to="/">
-        <div className="header__logo"><img src={Logo} alt="Logo" /></div>
-      </NavLink>
-        <label>
-          <input type="checkbox" />
-          <span className='check'></span>
-        </label>
-      <i onClick={handleHamClick} className="fa-solid fa-bars header__menu-ham"></i>
-      <nav ref={navbar} className="header__nav header__nav--close">
-      
-        <ul className="header__list">
-            <li className="header__item">
-          <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/">Home</NavLink>
+<header>
+  <div className="navbar">
+    <div className="logo">
+    <NavLink to="/">
+        <img src={Logo} alt="Logo" />
+      </NavLink>  
+    </div>
+    <ul className="links">
+            <li>
+          <NavLink className="link" to="/">Home</NavLink>
             </li>
-            <li className="header__item">
-          <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/AboutMe">About Me</NavLink>
+            <li>
+          <NavLink className="link" to="/AboutMe">About Me</NavLink>
             </li>
-            <li className="header__item">
-          <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/Habilities">Habilities</NavLink>
+            <li>
+          <NavLink className="link" to="/Habilities">Habilities</NavLink>
             </li>
-            <li className="nav-menu-item nav-link">
-          <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/MyProyects">My Projects</NavLink>
+            <li>
+          <NavLink className="link" to="/MyProyects">My Projects</NavLink>
             </li>
-            <li className="nav-menu-item nav-link">
-          <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/ContactMe">Contact Me</NavLink>
+            <li>
+          <NavLink className="link" to="/ContactMe">Contact Me</NavLink>
             </li>
         </ul>
-      </nav>
-    </header>
-    // <header className='header'>
-    //       <nav ref={navbar} className="nav header__nav--close">
-    //         <button className="nav-toogle">
-    //         <i onClick={handleHamClick} className="fa-solid fa-bars header__menu-ham"></i>
-    //         </button>
-    //       <NavLink className= {({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'}  to="/">
-    //         <img src={Logo} alt="Logo" />
-    //       </NavLink>
-    //       <ul className="nav-menu nav-menu_visible">
-    //       <li className="nav-menu-item nav-link">
-    //         <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/">Home</NavLink>
-    //       </li>
-    //       <li className="nav-menu-item nav-link">
-    //         <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/AboutMe">About Me</NavLink>
-    //       </li>
-    //       <li className="nav-menu-item nav-link">
-    //       <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/Habilities">Habilities</NavLink>
-    //       </li>
-    //       <li className="nav-menu-item nav-link">
-    //       <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/MyProyects">My Projects</NavLink>
-    //       </li>
-    //       <li className="nav-menu-item nav-link">
-    //       <NavLink className={({isActive}) => isActive ? 'header__navlink active-link' : 'header__navlink'} to="/ContactMe">Contact Me</NavLink>
-    //       </li>
-    //       </ul>
-    //       </nav>
-    // </header>
+          <NavLink className=' link action_btn' to="/">Descarga mi CV</NavLink>
+          <div className="toggle_btn">
+          <i onClick={handleHamClick} className="fa-solid fa-bars"></i>      
+          </div>
+          <div ref={navbar} className="dropdown_menu open">
+            <ul>
+          <li>
+          <NavLink className="link" to="/">Home</NavLink>
+            </li>
+            <li>
+          <NavLink className="link" to="/AboutMe">About Me</NavLink>
+            </li>
+            <li>
+          <NavLink className="link" to="/Habilities">Habilities</NavLink>
+            </li>
+            <li>
+          <NavLink className="link" to="/MyProyects">My Projects</NavLink>
+            </li>
+            <li>
+          <NavLink className="link" to="/ContactMe">Contact Me</NavLink>
+            </li>
+            <li>
+            <NavLink className=' link action_btn' to="/">Descarga mi CV</NavLink>
+            </li>
+            </ul>
+          </div>
+  </div>
+</header>
+
+        // <label>
+        //   <input type="checkbox" />
+        //   <span className='check'></span>
+        // </label>
+      
   )
 }
 
